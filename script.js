ffe6e333acc5fa5995480cf5d8a38463a6ab22e8
purva1353 <purvapatil1353@gmail.com>
2025-11-21 12:37:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Welcome message in console
+console.log("Welcome to My Portfolio Website!");
+
+// Welcome popup
+window.addEventListener("load", function () {
+    setTimeout(() => {
+        alert("Welcome to my Portfolio! 😊");
+    }, 500);
+});
